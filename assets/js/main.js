1e7cf477347a97f52051c0966553313b189d8abf
Mariana Algafy <mariana.algafy@gmail.com>
2023-02-12 19:08:18 +0200

    change theme color

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,3 +3,21 @@ function showSetting (){
     let settingSections = document.querySelector('.setting');
     settingSections.classList.toggle('active');
 }
+
+//Theme-Colors
+const colors = document.getElementsByClassName('colors');
+let i;
+for(i=0; i<colors.length; i++){
+    colors[i].addEventListener('click',changeColor)
+}
+
+function changeColor(){
+    let color = this.getAttribute('data-color');
+    document.documentElement.style.setProperty('--primary-color', color);
+}
+
+function getColorData(){
+    localStorage.getItem('theme-color');
+    console.log(localStorage.getItem('theme-color'));
+}
+
